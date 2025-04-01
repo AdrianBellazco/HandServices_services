f5efff48f17248ce73997595e2bcb8ee65d5da42
adrian <adrian.saavedra01@uceva.edu.co>
2025-03-31 21:45:22 -0500

    primer commit

---
+++ src/main/java/co/edu/uceva/handservice_services/HandServiceServicesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.handservice_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandServiceServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandServiceServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uceva/handservice_services/HandServiceServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.handservice_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandServiceServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
